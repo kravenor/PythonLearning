04558a7c15845ae0fe805c4b878e86d81b6b6042
Simone <simone.baldelli@codever.it>
2021-03-22 10:25:00 +0100

    First commit

---
+++ wheater.py
@@ -0,0 +1,17 @@
+import requests, json
+city='Montepulciano'
+api_key="8274ac5f6280135a33d5afa6e1fae4d3"
+base_url="https://api.openWeathermap.org/data/2.5/weather?"
+url=base_url+"appid="+api_key+"&q="+city
+response=requests.get(url)
+x=response.json()
+print(x)
+if x['cod']!=401:
+    print("city Name: ",x['name'])
+    print("Wheater: ",x['weather'])
+    print("Wheater: ",x['weather'][0]['main'])
+    print("Temp: ",x['main']['temp'])
+    print("Minimum Temp: ",x['main']['temp_min'])
+    print("Maximum Temp: ",x['main']['temp_max'])
+else:
+    print('City not found')
